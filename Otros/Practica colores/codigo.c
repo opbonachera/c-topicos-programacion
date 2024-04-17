8c2f486d8f4d82c2ccce9fe9037212f5b918a82f
opbonachera <obonachera@undav.edu.ar>
2024-04-16 23:29:24 -0300

    cambios

--- Otros/Practica colores/codigo.c
+++ Otros/Practica colores/codigo.c
@@ -4,7 +4,7 @@
 typedef struct
 {
     unsigned char pixel[3];
-    unsigned int profundidad;  // Esta estructura admite formatos de distinta profundidad de color, a priori utilizaremos s�lo 24 bits.
+    // unsigned int profundidad;  // Esta estructura admite formatos de distinta profundidad de color, a priori utilizaremos s�lo 24 bits.
 }t_pixel;
 
 typedef struct
@@ -18,13 +18,72 @@ typedef struct
 }t_metadata;
 
 void leerHeader();
-void printBmp();
+void printBmp(unsigned int, unsigned int);
+void printNumbers();
 
 int main()
 {
     leerHeader();
-    printBmp();
+    printBmp(240,360);
+    printNumbers();
+
     return 0;
+}
+
+
+void printBmp(unsigned int alto, unsigned int ancho) {
+    FILE *img, *nueva;
+    img = fopen("unlam.bmp", "rb");
+    nueva = fopen("nueva.bmp","wb");
+    t_pixel px;
+    t_metadata header;
+
+    if (img == NULL) {
+        perror("Error opening file");
+        return;
+    }
+    fseek(img, 54, SEEK_SET);
+
+    fseek(img, 2, SEEK_SET);
+    fread(&header.tamArchivo, sizeof(unsigned int), 1, img);
+    fwrite(&header.tamArchivo, sizeof(unsigned int),1, nueva);
+
+    fseek(img, 14, SEEK_SET);
+    fread(&header.tamEncabezado, sizeof(unsigned int), 1, img);
+    fwrite(&header.tamEncabezado, sizeof(unsigned int),1, nueva);
+
+    fseek(img, 10, SEEK_SET);
+    fread(&header.comienzoImagen, sizeof(unsigned int), 1, img);
+    fwrite(&header.comienzoImagen, sizeof(unsigned int),1, nueva);
+
+    fseek(img, 18, SEEK_SET);
+    fread(&header.ancho, sizeof(unsigned int), 1, img);
+    fread(&header.alto, sizeof(unsigned int), 1, img);
+    fwrite(&header.ancho, sizeof(unsigned int),1, nueva);
+    fwrite(&header.alto, sizeof(unsigned int),1, nueva);
+
+    for (int i = 0; i < alto; i++) {
+        for (int j = 0; j < ancho; j++) {
+            fread(&px.pixel, sizeof(px.pixel), 1, img);
+            fwrite(&px.pixel, sizeof(px.pixel), 1, nueva);
+        }
+    }
+
+    fclose(img);
+    fclose(nueva);
+}
+
+void printNumbers()
+{
+    unsigned char arr[3] = {0xFF, 0x32, 0x99};
+
+    for(int i = 0; i < 1 ; i++)
+    {
+        printf("Positive: %x\n", arr[i]);// 1111 1111 for 0xff 0000 0000
+        printf("Negative: %x\n", (unsigned char)~arr[i]); // 0011 0010 for 0x32 1100 1101
+
+        printf("Moved by two: %x \n", arr[1] >> 2);
+    }
 }
 
 void leerHeader()
@@ -56,45 +115,4 @@ void leerHeader()
     printf("Tamaño de cabecera: %u bytes\n", header.tamEncabezado);
     printf("Alto: %u bytes\n", header.alto);
     printf("Ancho: %u bytes\n", header.ancho);
-}
-
-void printBmp(unsigned int alto, unsigned int ancho) {
-    FILE *img;
-    img = fopen("unlam.bmp", "rb");
-
-    if (img == NULL) {
-        perror("Error opening file");
-        return;
-    }
-
-    t_pixel px;
-    fseek(img, 54, SEEK_SET);
-
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            fread(&px.pixel, sizeof(px.pixel), 1, img);
-            printf("%x, %x, %x\n",px.pixel[0], px.pixel[1], px.pixel[2]);
-            // AcA SE PODRIA ESCRIBIR
-        }
-    }
-    for (int i = 0; i < 100; i++) {
-        for (int j = 0; j < 100; j++) {
-            fread(&px.pixel, sizeof(px.pixel), 1, img);
-            printf("%x, %x, %x\n",px.pixel[0], px.pixel[1], px.pixel[2]);
-        }
-    }
-    fclose(img);
-}
-
-void printNumbers()
-{
-    unsigned char arr[3] = {0xFF, 0x32, 0x99};
-
-    for(int i = 0; i < 3 ; i++)
-    {
-        printf("Positive: %x\n", arr[i]);// 1111 1111 for 0xff 0000 0000
-        printf("Negative: %x\n", (unsigned char)~arr[i]); // 0011 0010 for 0x32 1100 1101
-
-        printf("Moved by two: %x \n", arr[1] >> 2);
-    }
 }
