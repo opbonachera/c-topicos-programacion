18f1531353615246d2ddd18a552ba3f575b954d9
Ornella Bonachera <obonachera@undav.edu>
2024-08-16 12:51:04 -0300

    tda fecha

--- Clases/2C Lunes1208/Lunes1208/main.c
+++ Clases/2C Lunes1208/Lunes1208/main.c
@@ -5,14 +5,24 @@
 
 int main()
 {
-    Fecha fecha;
+    /*Fecha fecha;
     ingresarFecha(&fecha);
 
     int dias = ingresarEnteroPositivo();
 
     Fecha fsuma = sumarDiasAFecha(&fecha, dias);
-
     mostrarFecha(&fsuma);
+    */
+
+    Fecha f1;
+    ingresarFecha(&f1);
+
+    Fecha f2;
+    ingresarFecha(&f2);
+
+    int difEntreFechas = diferenciaEntreFechas(&f1, &f2);
+
+    printf("La diferencia entre las fechas ingresadas en dias es de: %d dia(s) \n", difEntreFechas);
 
     return 0;
 }
