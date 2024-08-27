f8c29da2e6ee18599e511e1b0a3c42b83cc1140f
Ornella Bonachera <obonachera@undav.edu>
2024-08-27 15:45:52 -0300

    Strings!

--- Clases/2C Lunes2608 Strings/Strings/strings.c
+++ Clases/2C Lunes2608 Strings/Strings/strings.c
@@ -1,33 +1,84 @@
 
-char* normalizar(){
-    SecuenciaPalabra secLect, sectEscrt;
+char* normalizar(const char* cadenaANormalizar, const char* cadenaNormalizada){
+    SecuenciaPalabra secLect, sectEscr;
 
-    secPalCrear(&secLect, cadANormalizar);
-    secPalCrear(&secEscrt, cadANormalizar);
+    secPalCrear(&secLect, cadenaANormalizar);
+    secPalCrear(&secEscr, cadenaNormalizada);
 
     Palabra pal;
+
     secPalLeer(&secLect, &pal);
     while(!secPalFin(&secLect))
     {
         palabraATitulo(&pal);
-        secPalEscribir(&secEscrt, &pal);
-        secPalEscribir();
+        secPalEscribir(&secEscr, &pal);
+        secPalEscribir(&secEscr, ' ');
+        secPalLeer(&secLect, &pal);
     }
+
+    secPalCerrar(&secEscr);
+
+    return cadenaNormalizada;
 }
 
-bool secPalLeer(SecPal* sec, Palabra* pal)
+bool secPalLeer(SecuenciaPalabra* sec, Palabra* pal)
 {
-    while(*sec->cursor !='\0' && !esLetra(*sec->cursor))
+    while(*sec->cursor != '\0' && !esLetra(*sec->cursor))
     {
-        sec->cursor;
+        sec->cursor++;
     }
+
+    if(!(*sec->cursor))
+    {
+        sec->finSec = true;
+        return false;
+    }
+
+    char* actPal = pal->vectorPalabra;
+    while(*sec->cursor && esLetra(*sec->cursor))
+    {
+        *actPal=*sec->cursor;
+        actPal++;
+        sec->cursor++;
+    }
+
+    *actPal = '\0';
+    return true;
 }
 
-void secPalCrear(SecPal* sec, const char* cad)
+void secPalCrear(SecuenciaPalabra* sec, const char* cad)
 {
     sec->cursor = cad;
     sec->finSec = false;
 }
 
+void secPalEscribir(SecuenciaPalabra* sec, const Palabra* pal)
+{
+    char* actPal = pal->vectorPalabra;
+    while(*actPal)
+    {
+        *sec->cursor = *actPal;
+        sec->cursor++;
+        actPal++;
+    }
+}
 
+void secPalEscribirCar(SecuenciaPalabra* sec, char c)
+{
+    *sec->cursor = c;
+    sec->cursor++;
+}
 
+void palabraATitulo(Palabra *pal)
+{
+    char* actPal = pal->vectorPalabra;
+
+    *actPal = aMayuscula(*actPal);
+    actPal++;
+
+    while(*actPal)
+    {
+        *actPal = aMinuscula(&actPal);
+        actPal++;
+    }
+}
--- Clases/2C Lunes2608 Strings/Strings/strings.h
+++ Clases/2C Lunes2608 Strings/Strings/strings.h
@@ -1,19 +1,39 @@
 #ifndef STRINGS_H_INCLUDED
 #define STRINGS_H_INCLUDED
 
-#define TAM_PAL 31
+#include <stdbool.h>
+
+#define TAM_PALABRA 31
+
+#define esLetra(c)(((c)>= 'A' && (c) <= 'z') || ((c) >='a' && (c) <= 'z'))
+#define aMayuscula(c)( (c) >= 'a' && (c) <= 'z' ? (c) - ('a' - 'A') : (c))
+#define aMinuscula(c)( ((c) >= 'A' && (c) <= 'Z') ? ((c) + ('a' - 'A')) : (c))
+
 
 typedef struct
 {
     char* cursor;
     bool  finSec;
-}SecPal;
+}
+SecuenciaPalabra;
 
 typedef struct
 {
-    char vPal[TAM_PAL];
-}Palabra;
+    char vectorPalabra[TAM_PALABRA];
+}
+Palabra;
 
 int normalizar(cadANormalizar, cadNormalizada);
+void secPalCrear(SecuenciaPalabra* sec, Palabra *palabra);
+bool secPalLeer(SecuenciaPalabra* sec, Palabra *palabra);
+void secPalEscribir(SecuenciaPalabra* sec, const Palabra* pal);
+void secPalEscribirCar(SecuenciaPalabra* sec, char c);
+void secPalCerrar(SecuenciaPalabra* sec);
+void palabraATitulo(Palabra* palabra);
+
+bool esLetra(char c);
+char aMayuscula(char c);
+char aMinuscula(char c);
+
 
 #endif // STRINGS_H_INCLUDED
