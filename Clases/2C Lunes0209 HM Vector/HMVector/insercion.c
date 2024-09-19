75f7714a4fd7b6c853df40040e7104b7d6caf567
Ornella Bonachera <obonachera@undav.edu>
2024-09-19 14:48:46 -0300

    Ordenamiento

--- Clases/2C Lunes0209 HM Vector/HMVector/insercion.c
+++ Clases/2C Lunes0209 HM Vector/HMVector/insercion.c
@@ -5,14 +5,14 @@ void ordenarVectorInsercion(Vector* vector)
 {
     for(int i=1; i<vector->ce; i++)
     {
-        int pos = vector->vec[i];
-        int j= i - 1;
+        int element = vector->vec[i];
+        int j= i - 1; // Element to the left of i
 
-        while (j >= 0 && vector->vec[j] > pos) {
-            vector->vec[j + 1] = vector->vec[j];
-            j = j - 1;
+        while (j >= 0 && vector->vec[j] > element) {
+            vector->vec[j + 1] = vector->vec[j]; // Move all elements to the right
+            j--;
         }
-        vector->vec[j + 1] = pos;
+        vector->vec[j + 1] = element;
 
     }
 }
--- Clases/2C Lunes0209 HM Vector/HMVector/qsort.c
+++ Clases/2C Lunes0209 HM Vector/HMVector/qsort.c
@@ -1,18 +1,39 @@
 #include "hmvector.h"
 #include "ordenamiento.h"
 
-void ordenarVectorInsercion(Vector* vector)
+void ordenarVectorQsort(Vector* vector, int inicio, int fin)
 {
-    for(int i=1; i<vector->ce; i++)
+    if( fin <= inicio)
+        return; // No more available partitions
+
+    int pivot = partition(vector, inicio, fin);
+
+    ordenarVectorQsort(vector, start, pivot - 1); // List of numbers to the left of pivot
+    ordenarVectorQsort(vector, pivot + 1, end); // List of numbers to the right of pivot
+
+}
+
+void partition(Vector* vector, int inicio, int fin)
+{
+    int pivot = vector->vec[end]; // Always take last element of array as pivot
+    int i = start - 1; // First element on the list
+
+    for(int j=start; j<=end, j++)
     {
-        int pos = vector->vec[i];
-        int j= i - 1;
-
-        while (j >= 0 && vector->vec[j] > pos) {
-            vector->vec[j + 1] = vector->vec[j];
-            j = j - 1;
+        if(array[j] < pivot)
+        {
+            i++;
+            int temp = vector->vec[i];
+            vector->vec[i] = vector->vec[j];
+            vector->vec[j] = temp;
         }
-        vector->vec[j + 1] = pos;
-
     }
+
+    i++;
+    // Change pivot position
+    int temp = vector->vec[i];
+    vector->vec[i] = vector->vec[end];
+    vector->vec[end] = temp;
+
+    return i;
 }
