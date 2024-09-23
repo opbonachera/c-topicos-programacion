84091ebe447612074e8403978043b970aa8b92db
Ornella Bonachera <obonachera@undav.edu>
2024-09-23 11:47:15 -0300

    Reestructuracion de carpetas

---
+++ Temas/TpPractica/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "funciones.h"
+
+int main()
+{
+    // copiarImagen();
+    //recortarImagen();
+    //rotarImagenDerecha();
+     //rotarImagenIzquierda();
+    //rotarMatrizIzquierda();
+    //transposeMatrix();
+    //espejarVertical();
+    //espejarHorizontal();
+    //espejarImagenHorizontal();
+    //espejarImagenVertical();
+    //concatenarMatrizHorizontal();
+    //concatenarMatrizVertical();
+    //concatenarImagenHorizontal();
+    //rellenarImagen();
+    pixelearImagen();
+    return 0;
+}
